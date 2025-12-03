c7373109ff8ff2369623511fe338f44ee6772103
Janodh <69080518+Janodh@users.noreply.github.com>
2025-12-03 14:48:16 +0530

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import Footer from "../components/Footer";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import Script from "next/script";
 
 export const metadata = {
   title: "Beach Walk Tours",
@@ -14,10 +15,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <script
-          src="https://www.google.com/recaptcha/api.js?render=6LeGmxgsAAAAAL_OhPQVlaPjL-4ioJln-A5uStEQ"
-          async
-        ></script>
+        <header>
+          <script
+            src="https://www.google.com/recaptcha/api.js?render=6LeGmxgsAAAAAL_OhPQVlaPjL-4ioJln-A5uStEQ"
+            async
+          ></script>
+          <script
+            id="cookieyes"
+            type="text/javascript"
+            src="https://cdn-cookieyes.com/client_data/deaead8ee1b2af39f1ac25f3a71e8c9b/script.js"
+            strategy="beforeInteractive"
+          ></script>
+        </header>
+
         <Navbar />
         {children}
         <Footer />
